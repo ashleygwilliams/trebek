88d39b80d9afccb9c2357739ee70c34649734506
ashley williams <heyashleyashley@gmail.com>
2014-01-29 17:22:54 -0500

    the rest of it

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-trebek
-======
+#gambit + trebek
 
-a framework for building and serving jeopardy games
+
+## gambit is a server side application that allows you to design cards and build decks and serve them to a client
+
+## trebek is a clientside library that takes cards built in gambit and serves up an interactive jeopardy game
+
--- app.rb
+++ app.rb
@@ -1,15 +1,25 @@
 require "bundler"
 Bundler.require
 
-class Trebek < Sinatra::Application
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
 
-    configure do
-      set :root, File.dirname(__FILE__)
-      set :public_folder, 'public'
-    end
+class Gambit < Sinatra::Application
+
+  configure do
+    set :root, File.dirname(__FILE__)
+    set :public_folder, 'public'
+  end
 
   get "/" do
     haml :index
+  end
+
+  get "/deck/:file_name" do
+    deck = Deck.new("data/#{params[:file_name]}.yml")
+    @cards = deck.cards
+    haml :deck
   end
 
   get "/styles.css" do
@@ -18,9 +28,25 @@ class Trebek < Sinatra::Application
 
   helpers do
 
-    def partial(file)
-      haml :"_#{file}", :layout => false
+    def partial(template,locals=nil)
+      if template.is_a?(String) || template.is_a?(Symbol)
+        template = :"_#{template}"
+      else
+        locals=template
+        template = template.is_a?(Array) ? :"_#{template.first.class.to_s.downcase}" : :"_#{template.class.to_s.downcase}"
+      end
+      if locals.is_a?(Hash)
+        haml template, {}, locals      
+      elsif locals
+        locals=[locals] unless locals.respond_to?(:inject)
+        locals.inject([]) do |output,element|
+          output << haml(template,{template.to_s.delete("_").to_sym => element})
+        end.join("\n")
+      else 
+        haml template
+      end
     end
+    
   end
 
 end
