71958f05f63e642ce59e56232ab22e698fc1748c
Kate <katepdonahue@gmail.com>
2014-01-31 08:52:56 -0500

    Nest chosen handler

--- public/trebek2.js
+++ public/trebek2.js
@@ -5,14 +5,12 @@ $(".card").not(".cat-card").on('click', function(){
   $this.find(".question").show();
   // $this.children.toggle(); 
   $(".card").not(".cat-card").not($this).addClass("disabled");
-  // $this.off('click', 'chosen', function() {
-  //   $(this).removeClass("chosen");
-  // });
+  $(".chosen").on('click', function() {
+    var $this = $(this);
+    $this.find(".answer").show();
+    $this.find(".question").hide();
+  });
 });
 
-$(".chosen").on('click', function() {
-  var $this = $(this);
-  $this.find(".answer").show();
-  $this.find(".question").hide();
-});
+
 
