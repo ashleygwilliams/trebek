4fbc75cb71679713f1eca4f024d51849651cbf9b
Nikki Thean <nikki.thean@gmail.com>
2014-01-30 20:48:06 -0500

    Refactor for performance... I hope

--- public/nikliver.js
+++ public/nikliver.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   var $cards = $(".card").not(".cat-card");
   var $buttons = $(".player button");
-  var value;
+  var cardValue;
 
   $cards.on("click", function(){
     var $this = $(this);
-    var $value = $this.children().eq(0);
-    var $question = $this.children().eq(1);
-    var $answer = $this.children().eq(2);
+    var $value = $this.find(".value");
+    var $question = $this.find(".question");
+    var $answer = $this.find(".answer");
 
     if (!isDisabled($this)) {
       if (isVisible($value)) {
@@ -16,10 +16,10 @@ $(document).ready(function() {
         $this.addClass("chosen");
         $buttons.removeClass("disabled");
         $cards.not(this).addClass("disabled");
+        cardValue = parseInt($value.text());
       } else if (isVisible($question)) {
         $question.fadeOut();
         $answer.fadeIn();
-        value = parseInt($value.text());
       } else {
         $answer.fadeOut();
         $this.removeClass("chosen").addClass("played");
@@ -52,10 +52,12 @@ $(document).ready(function() {
   function changeScore(object, action) {
     var $container = object.parent().find("h2");
     var score = parseInt($container.text());
+    console.log(score);
+    console.log(cardValue);
     if (action === "increase") {
-      var newTotal = $container.text(score + value);
+      var newTotal = $container.text(score + cardValue);
     } else if (action === "decrease") {
-      var newTotal = $container.text(score - value);
+      var newTotal = $container.text(score - cardValue);
     }
   }
 });
