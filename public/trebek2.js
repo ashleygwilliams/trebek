2753688fc2c7ac74c8068ea21e0e730c4567a1b2
Kate <katepdonahue@gmail.com>
2014-01-31 09:24:26 -0500

    Add if statment to prevent picking other cards while one card is chosen

--- public/trebek2.js
+++ public/trebek2.js
@@ -1,22 +1,23 @@
 $(".card").not(".cat-card").on('click', function(){
   var $this = $(this);
-  $this.addClass("chosen");
-  $this.find(".value").hide();
-  $this.find(".question").show();
-  // $this.children.toggle(); 
-  $(".card").not(".cat-card").not($this).addClass("disabled");
-  $(".chosen").on('click', function() {
-    var $this = $(this);
-    $this.find(".answer").show();
-    $this.find(".question").hide();
-    $(".score").find("button").removeClass("disabled");
+  if(!$this.hasClass("disabled")) {
+    $this.addClass("chosen");
+    $this.find(".value").hide();
+    $this.find(".question").show();
+    $(".card").not(".cat-card").not($this).addClass("disabled");
     $(".chosen").on('click', function() {
       var $this = $(this);
-      $this.find(".answer").hide();
-      $this.addClass("played");
-      $(".card").not(".cat-card").not($this).removeClass("disabled");
+      $this.find(".answer").show();
+      $this.find(".question").hide();
+      $(".score").find("button").removeClass("disabled");
+      $(".chosen").on('click', function() {
+        var $this = $(this);
+        $this.find(".answer").hide();
+        $this.addClass("played");
+        $(".card").not(".cat-card").not($this).removeClass("disabled");
+      });
     });
-  });
+  }
 });
 
 $(".score").children().on("click", function() {
