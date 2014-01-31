79949c1378386c480a163c74f2efb4f53c433224
Kate <katepdonahue@gmail.com>
2014-01-31 13:27:06 -0500

    If buttons are disabled they should not be able to be clicked

--- public/trebek2.js
+++ public/trebek2.js
@@ -1,17 +1,20 @@
-$(".card").not(".cat-card").on('click', function(){
+var $playCards = $(".card").not(".cat-card");
+var $counter = $(".score").find("button");
+
+$playCards.on('click', function(){
   var $this = $(this);
   if(!$this.hasClass("disabled")) {
     $this.addClass("chosen");
     $this.find(".value").hide();
     $this.find(".question").show();
-    $(".card").not(".cat-card").not($this).addClass("disabled");
+    $playCards.not($this).addClass("disabled");
     $(".chosen").on('click', function() {
       var $this = $(this);
       $this.find(".answer").show();
       $this.find(".question").hide();
-      $(".score").find("button").removeClass("disabled");
+      $counter.removeClass("disabled");
       $(".chosen").on('click', function() {
-        if(!$(".score").find("button").hasClass("disabled")) {
+        if(!$counter.hasClass("disabled")) {
           alert("Please give out the points!");
         }
       });
@@ -22,22 +25,25 @@ $(".card").not(".cat-card").on('click', function(){
 
 $(".score").children().on("click", function() {
   var $this = $(this);
-  var $score = Number($this.parent().find("h2").text());
-  var $points = Number($(".chosen").find(".value").text());
-  if($this.hasClass("up")) {
-    $score += $points;
-    $this.parent().find("h2").text(String($score));
+  if(!$this.hasClass("disabled")) {
+    var $score = Number($this.parent().find("h2").text());
+    var $points = Number($(".chosen").find(".value").text());
+    if($this.hasClass("up")) {
+      $score += $points;
+      $this.parent().find("h2").text(String($score));
+    }
+    else {
+      $score -= $points;
+      $this.parent().find("h2").text(String($score));
+    }
+    $counter.addClass("disabled");
+    var $card = $(".chosen");
+    $card.find(".answer").hide();
+    $card.removeClass("chosen");
+    $card.addClass("played");
+    $playCards.not($card).removeClass("disabled");
+    $counter.addClass("disabled");
   }
-  else {
-    $score -= $points;
-    $this.parent().find("h2").text(String($score));
-  }
-  $(".score").find("button").addClass("disabled");
-  var $card = $(".chosen");
-  $card.find(".answer").hide();
-  $card.addClass("played");
-  $(".card").not(".cat-card").not($card).removeClass("disabled");
-  $(".score").find("button").addClass("disabled");
 });
 
 
