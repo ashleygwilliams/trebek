3fa8cf63a19ec6d1d08109af05ee977e1684d66d
Sara Gorecki <sgorecki@gmail.com>
2014-01-30 23:49:43 -0500

    fix variable assignment

--- public/studentTrebek.js
+++ public/studentTrebek.js
@@ -2,18 +2,19 @@ $(document).ready(function(){
   var $card = $(".card").not(".cat-card");
 
   $card.on("click", function(){
-    $this = $(this);
-    $other = $card.not($this).not(".played");
-    $disabled = $this.hasClass("disabled");
-    $question = $this.children().filter(".question");
-    $answer = $this.children().filter(".answer");
-    $value = $this.children().filter(".value");
+    var $this = $(this);
+    var $other = $card.not($this).not(".played");
+    var $disabled = $this.hasClass("disabled");
+    var $question = $this.children().filter(".question");
+    var $answer = $this.children().filter(".answer");
+    var $value = $this.children().filter(".value");
 
     if (!$disabled && $value.attr("style") == null){
       $other.addClass("disabled");
       $this.addClass("chosen");
       $value.attr("style", "display: none");
       $question.attr("style", "display: block");
+      $("button").removeClass("disabled");
     } else if (!$disabled && $question.attr("style") == "display: block"){
       $question.attr("style", "display: none");
       $answer.attr("style", "display: block");
@@ -21,6 +22,7 @@ $(document).ready(function(){
       $answer.attr("style", "display: none");
       $this.addClass("disabled played");
       $other.removeClass("disabled");
+      $("button").addClass("disabled");
     }
   });
 
