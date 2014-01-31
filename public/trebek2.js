47287d7a2d791b667927870772e922430d0eea70
Kate <katepdonahue@gmail.com>
2014-01-31 14:52:13 -0500

    Use conditions instead of nesting hondlers

--- public/trebek2.js
+++ public/trebek2.js
@@ -1,50 +1,53 @@
-var $playCards = $(".card").not(".cat-card");
-var $counter = $(".score").find("button");
+$(document).ready(function() {
 
-$playCards.on('click', function(){
-  var $this = $(this);
-  if(!$this.hasClass("disabled")) {
-    $this.addClass("chosen");
-    $this.find(".value").hide();
-    $this.find(".question").show();
-    $playCards.not($this).addClass("disabled");
-    $(".chosen").on('click', function() {
-      var $this = $(this);
-      $this.find(".answer").show();
-      $this.find(".question").hide();
-      $counter.removeClass("disabled");
-      $(".chosen").on('click', function() {
-        if(!$counter.hasClass("disabled")) {
+  var $playCards = $("div.card").not(".cat-card");
+  var $counter = $("div.score button");
+
+  $playCards.on('click', function(){
+    var $this = $(this);
+    if(!$this.hasClass("disabled")) {
+      if(!$this.hasClass("chosen")) {
+        $this.addClass("chosen");
+        $this.find("div.value").fadeOut();
+        $this.find("div.question").fadeIn();
+        $playCards.not($this).addClass("disabled");
+      }
+      else if($this.find("div.question").is(":visible")) {
+        $this.find("div.answer").fadeIn();
+        $this.find("div.question").fadeOut();
+        $counter.removeClass("disabled");
+      }
+      else {
+        // if(!$counter.hasClass("disabled")) {
           alert("Please give out the points!");
-        }
-      });
-    });
-  }
-});
-
-
-$(".score").children().on("click", function() {
-  var $this = $(this);
-  if(!$this.hasClass("disabled")) {
-    var $score = Number($this.parent().find("h2").text());
-    var $points = Number($(".chosen").find(".value").text());
-    if($this.hasClass("up")) {
-      $score += $points;
-      $this.parent().find("h2").text(String($score));
+        // }
+      }
     }
-    else {
-      $score -= $points;
-      $this.parent().find("h2").text(String($score));
+  });
+
+  $("div.score").children().on("click", function() {
+    var $this = $(this);
+    if(!$this.hasClass("disabled")) {
+      var $score = Number($this.parent().find("h2").text());
+      var $points = Number($("div.chosen .value").text());
+      if($this.hasClass("up")) {
+        $score += $points;
+        $this.parent().find("h2").text(String($score));
+      }
+      else {
+        $score -= $points;
+        $this.parent().find("h2").text(String($score));
+      }
+      $counter.addClass("disabled");
+      var $card = $("div.chosen");
+      $card.find("div.answer").hide();
+      $card.removeClass("chosen");
+      $card.addClass("played");
+      $card.addClass("disabled");
+      $playCards.not($card).removeClass("disabled");
+      $counter.addClass("disabled");
     }
-    $counter.addClass("disabled");
-    var $card = $(".chosen");
-    $card.find(".answer").hide();
-    $card.removeClass("chosen");
-    $card.addClass("played");
-    $playCards.not($card).removeClass("disabled");
-    $counter.addClass("disabled");
-  }
+  });
+
 });
-
-
 
