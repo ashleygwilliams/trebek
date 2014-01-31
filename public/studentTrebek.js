63a363dbc43a163b9e107e6f132e44e8de6cbfc7
Sara Gorecki <sgorecki@gmail.com>
2014-01-30 23:17:17 -0500

    create variables for queries

--- public/studentTrebek.js
+++ public/studentTrebek.js
@@ -2,9 +2,15 @@ $(document).ready(function(){
   var $card = $(".card").not(".cat-card");
 
   $card.on("click", function(){
-    if (!($(this).hasClass("disabled"))){
-      $card.not($(this)).addClass("disabled");
-      $(this).addClass("chosen");  
+    $this = $(this);
+    $question = $this.children().filter(".question").text();
+    $answer = $this.children().filter(".answer").text();
+    $value = $this.children().filter(".value").text();
+
+    if (!($this.hasClass("disabled"))){
+      $card.not($this).addClass("disabled");
+      $this.addClass("chosen");  
+      
       
     }
     
