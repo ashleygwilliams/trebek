6cd0311eb7a1019320bd42ecfd2b197859e01063
nishacodes <nishabatra27@gmail.com>
2014-01-31 11:06:36 -0500

    fix logic to not be able to click on disabled cards

--- public/jeopardy.js
+++ public/jeopardy.js
@@ -1,51 +1,50 @@
-var $cards = $(".card").not(".cat-card,.played,.disabled");
-var $buttons = $(".score button");
-var p1score = 0;
-var p2score = 0;
-var p3score = 0;
-var wager;
-var counter = 0;
-// var $score = $()
+$(document).ready(function(){
+  var $cards = $(".card").not(".cat-card,.played,.disabled");
+  var $buttons = $(".score button");
+  var wager;
+  var counter = 0;
 
+  $cards.on("click", function(){
+    var $this = $(this);
+    var $value = $this.children(".value");
+    var $question = $this.children(".question");
+    var $answer = $this.children(".answer");
+    var $disabled = $this.hasClass("disabled");
+      if (!$disabled){
+        if (counter == 0) {
+          wager = Number($this.find(".value").text());
+          $this.addClass("chosen");
+          $value.fadeOut();
+          $question.fadeIn();
+          $cards.not($this).addClass("disabled");
+          $buttons.removeClass("disabled");
+          counter ++;
+        } else if (counter == 1) {
+          $question.fadeOut();
+          $answer.fadeIn();
+          counter ++;
+        } else {
+          $answer.fadeOut();
+          $this.removeClass("chosen").addClass("played");
+          $cards.not($this).removeClass("disabled");
+          counter = 0;
+        };
+      }
+  });
 
+  $buttons.on("click", function(){
+    var $this = $(this);
+    var $score = Number($this.parent().find("h2").text());
 
-$cards.on("click", function(){
-  var $this = $(this);
-  var $value = $this.children(".value");
-  var $question = $this.children(".question");
-  var $answer = $this.children(".answer");
-  console.log($this);
-    if (counter == 0) {
-      wager = Number($this.find(".value").text());
-      $this.addClass("chosen");
-      $value.fadeOut();
-      $question.fadeIn();
-      $cards.not($this).addClass("disabled");
-      $buttons.removeClass("disabled");
-      counter ++;
-    } else if (counter == 1) {
-      $question.fadeOut();
-      $answer.fadeIn();
-      counter ++;
+    $buttons.addClass("disabled");
+    if ($this.hasClass("up")) {
+      $score = $score + wager;
     } else {
-      $answer.fadeOut();
-      $this.removeClass("chosen").addClass("played");
-      $cards.not($this).removeClass("disabled");
-      counter = 0;
-    };
-  
+      $score = $score - wager;
+    }
+    
+    $this.parent().find("h2").text($score);
+  });
+
+
 });
-
-$buttons.on("click", function(){
-  var $this = $(this);
-  $buttons.addClass("disabled");
-  var $score = Number($this.parent().find("h2").text());
-  if ($this.hasClass("up")) {
-    $score = $score + wager;
-  } else {
-    $score = $score - wager;
-  }
-  $this.parent().find("h2").text($score);
-});
-
-
