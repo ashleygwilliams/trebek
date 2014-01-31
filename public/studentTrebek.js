9038cba39b09f160404dd6a154c0659d16db66da
Sara Gorecki <sgorecki@gmail.com>
2014-01-30 23:46:05 -0500

    complete chain of click events to cycle through card states

--- public/studentTrebek.js
+++ public/studentTrebek.js
@@ -3,17 +3,25 @@ $(document).ready(function(){
 
   $card.on("click", function(){
     $this = $(this);
-    $question = $this.children().filter(".question").text();
-    $answer = $this.children().filter(".answer").text();
-    $value = $this.children().filter(".value").text();
+    $other = $card.not($this).not(".played");
+    $disabled = $this.hasClass("disabled");
+    $question = $this.children().filter(".question");
+    $answer = $this.children().filter(".answer");
+    $value = $this.children().filter(".value");
 
-    if (!($this.hasClass("disabled"))){
-      $card.not($this).addClass("disabled");
-      $this.addClass("chosen");  
-      
-      
+    if (!$disabled && $value.attr("style") == null){
+      $other.addClass("disabled");
+      $this.addClass("chosen");
+      $value.attr("style", "display: none");
+      $question.attr("style", "display: block");
+    } else if (!$disabled && $question.attr("style") == "display: block"){
+      $question.attr("style", "display: none");
+      $answer.attr("style", "display: block");
+    } else if (!$disabled && $answer.attr("style") == "display: block"){
+      $answer.attr("style", "display: none");
+      $this.addClass("disabled played");
+      $other.removeClass("disabled");
     }
-    
   });
 
 });
