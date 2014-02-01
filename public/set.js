519bb0db27602a606ab9d967e83403d128ce0d78
Nikki Thean <nikki.thean@gmail.com>
2014-02-01 10:29:46 -0500

    IT WORKS

---
+++ public/set.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  var $card = $(".card");
+
+  $card.on("click", function() {
+    var $this = $(this);
+    var $selected;
+
+    $this.toggleClass("selected");
+    $selected = $(".selected");
+
+    if ($selected.length === 3) {
+      if (isASet($selected)) {
+        alert("SET!");
+      } else {
+        alert("Not a set.")
+      }
+      $card.removeClass("selected");
+    }
+  });
+});
+
+function isASet(cards) {
+  // collect card data attributes and put into arrays and uniq. if length of any is 2, it is not a set.
+
+  var numbers = [];
+  var shapes = [];
+  var shades = [];
+  var colors = [];
+
+  cards.each(function () {
+    var $this = $(this);
+    numbers.push($this.data("number"));
+    shapes.push($this.data("shape"));
+    shades.push($this.data("shade"));
+    colors.push($this.data("color"));
+  });
+
+  return check();
+
+  function check() {
+    if (jQuery.unique(numbers).length === 2 || jQuery.unique(shapes).length === 2 || jQuery.unique(shades).length === 2 || jQuery.unique(colors).length === 2) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
