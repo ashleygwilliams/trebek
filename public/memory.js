bf32210b80d7b0f6c5f5c791dd5ff0ff462d60b2
sranso <sranso@gmail.com>
2014-02-02 12:30:45 -0500

    if two cards are the same, they stay face up

--- public/memory.js
+++ public/memory.js
@@ -11,13 +11,22 @@ $(document).ready(function(){
         $this.addClass("chosen");
         $this.find(".question").fadeIn();
         $this.find(".value").fadeOut();
-        setTimeout(function(){
-          $this.find(".value").fadeIn();
-          $this.find(".question").fadeOut();
-          $otherCard.find(".value").fadeIn();
-          $otherCard.find(".question").fadeOut();
-        }, 3000);
-      } else {
+        if ($otherCard.find(".question").text() == $this.find(".question").text()) {
+          setTimeout(function(){
+            $this.removeClass("chosen");
+            $otherCard.removeClass("chosen");
+          }, 1000);
+        } else {
+          setTimeout(function(){
+            $this.find(".value").fadeIn()
+            $this.find(".question").fadeOut();
+            $this.removeClass("chosen");
+            $otherCard.find(".value").fadeIn();
+            $otherCard.find(".question").fadeOut();
+            $otherCard.removeClass("chosen");
+          }, 2000);
+        };
+      } else if ($faceUpCards.length < 1) {
         $this.addClass("chosen");
         $this.find(".question").fadeIn();
         $this.find(".value").fadeOut();
