7cdfa478d7b71995cd5a09d3a8725859a5582096
Sara Gorecki <sgorecki@gmail.com>
2014-02-03 13:09:25 -0500

    add ability to select cards if they are not facedown

--- public/spider.js
+++ public/spider.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
   var $columnArray = $(".column")
+  
+  //flip last row in each column face-up to start game
   $columnArray.each(function(col){
     $(this).children().last().removeClass("faceDown");
   })
+
+  //select card on click
+  $(".card").on("click", function(){
+    var $this = $(this);
+
+    if(!$this.hasClass("faceDown")){
+      //select card and cards below
+      $this.addClass("selected");
+      $this.nextAll().addClass("selected");
+    }
+  });
+
 });
