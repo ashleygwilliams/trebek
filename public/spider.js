5cc28659a4cfbf6dae2482fa757cdab878aea4b5
Sara Gorecki <sgorecki@gmail.com>
2014-02-04 13:26:49 -0500

    separate out events for if there is a selection or not, and add logic to move cards between columns and flip cards face-up

--- public/spider.js
+++ public/spider.js
@@ -9,12 +9,30 @@ $(document).ready(function(){
   //select card on click
   $(".card").on("click", function(){
     var $this = $(this);
+    var $selected = $(".selected");
 
-    if(!$this.hasClass("faceDown")){
-      //select card and cards below
-      $this.addClass("selected");
-      $this.nextAll().addClass("selected");
+    if(!$this.hasClass("faceDown") && !$this.hasClass("blocked")){
+      //for when nothing has been selected
+      if($selected.length == 0){
+        //select card and cards below
+        $this.addClass("selected");
+        $this.nextAll().addClass("selected");
+      } else {
+      // for when cards have been selected
+        if($this.hasClass("selected")){
+          //remove selected if clicked on selected
+          $selected.removeClass("selected");
+        } else {
+          //move selected card to new column
+          $selected.prev().removeClass("faceDown");
+          $selected.appendTo($this.parent());
+          $(".card").removeClass("selected");
+
+        }
+      }
     }
   });
 
 });
+
+//need to make reserve not-selectable
